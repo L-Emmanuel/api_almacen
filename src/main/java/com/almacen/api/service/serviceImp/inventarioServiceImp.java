1fc49f3953a08741777c4fd91d206cc1917e4916
Emmanuel Valdez <emanuelstroke1@gmail.com>
2025-04-15 15:15:17 -0600

    Agrego clases y estructura del proyecto Java

--- src/main/java/com/almacen/api/service/serviceImp/inventarioServiceImp.java
+++ src/main/java/com/almacen/api/service/serviceImp/inventarioServiceImp.java
@@ -4,11 +4,16 @@ import com.almacen.api.models.Inventario;
 import com.almacen.api.payloads.Dto.InventarioDto;
 import com.almacen.api.payloads.Dto.Pagination;
 import com.almacen.api.service.inventarioService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
-
+@Service
+@RequiredArgsConstructor
 public class inventarioServiceImp implements inventarioService {
+
+
     @Override
     public Pagination<InventarioDto> findAll() {
         return null;
